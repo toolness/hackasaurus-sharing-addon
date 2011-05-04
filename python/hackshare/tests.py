baf0d40dc5ec8c77014be445494417f0d1d8e668
Atul Varma <varmaa@gmail.com>
2011-05-04 16:36:14 -0700

    Added basic flickr uploader implementation and test.

--- python/hackshare/tests.py
+++ python/hackshare/tests.py
@@ -1,13 +1,20 @@
 import os
 
+from django.conf import settings
 from django.test import TestCase
 from django.test.client import Client
+from django.utils import simplejson as json
+import views
 import flickr
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 path = lambda *x: os.path.join(ROOT, *x)
 
 SAMPLE_IMG = path('sample_data', 'test_image.png')
+SAMPLE_INDEX = path('sample_data', 'index.html')
+
+class FakeFlickr(object):
+    pass
 
 class FlickrTests(TestCase):
     def test_flickr_token_is_valid(self):
@@ -23,7 +30,39 @@ class FlickrTests(TestCase):
                          'http://flic.kr/p/9EFw7o')
 
 class ApiTests(TestCase):
+    def setUp(self):
+        self._oldflickr = views.flickr
+        self.flickr = FakeFlickr()
+        views.flickr = self.flickr
+        
+    def tearDown(self):
+        views.flickr = self._oldflickr
+
     def test_upload_returns_method_not_allowed_on_get(self):
         c = Client()
         response = c.get('/upload/')
         self.assertEqual(response.status_code, 405)
+
+    def test_upload_succeeds_when_minimal_args_are_valid(self):
+        c = Client()
+
+        def fake_upload(filename, **kwargs):
+            self.assertFalse(hasattr(self.flickr, 'filename'))
+            self.assertTrue(os.path.isfile(filename))
+            self.assertEqual(open(SAMPLE_IMG, 'rb').read(),
+                             open(filename, 'rb').read())
+            self.flickr.filename = filename
+            return 'fake photo id'
+
+        self.flickr.upload = fake_upload
+
+        response = c.post('/upload/', dict(
+            screenshot=open(SAMPLE_IMG, 'rb'),
+            auth_token=settings.UPLOAD_AUTH_TOKEN,
+            #index_html=open(SAMPLE_INDEX, 'rb'),
+            #additional_file_count='0'
+        ))
+        self.assertEqual(response['content-type'], 'application/json')
+        self.assertFalse(os.path.exists(self.flickr.filename))
+        obj = json.loads(response.content)
+        self.assertEqual(obj['photo_id'], 'fake photo id')
--- python/hackshare/views.py
+++ python/hackshare/views.py
@@ -1,7 +1,40 @@
-from django.http import HttpResponseNotAllowed
+import os
+import tempfile
+
+from django.http import HttpResponse, HttpResponseNotAllowed
+from django.utils import simplejson as json
+import flickr
+
+def json_response(**obj):
+    return HttpResponse(json.dumps(obj), mimetype='application/json')
+
+def upload_screenshot_to_flickr(req):
+    fd, tempfilename = tempfile.mkstemp(suffix='.png')
+    os.close(fd)
+    f = open(tempfilename, 'wb')
+    for chunk in req.FILES['screenshot'].chunks():
+        f.write(chunk)
+    f.close()
+    
+    try:
+        photo_id = flickr.upload(
+            filename=tempfilename,
+            title=req.POST.get('title', 'Untitled'),
+            description=req.POST.get('description', ''),
+            content_type=2
+            )
+    finally:
+        os.remove(tempfilename)
+
+    return photo_id
 
 # Create your views here.
 def upload(req):
     if req.method != 'POST':
         return HttpResponseNotAllowed(['POST'])
-    return HttpResponse('hi')
+
+    photo_id = upload_screenshot_to_flickr(req)
+
+    return json_response(
+        photo_id=photo_id
+        )
